6b4af045f6ac8997f94ec80bb1e408ab1ff2ddc0
Ethan <ewood17@huskers.unl.edu>
2026-03-19 10:04:11 -0500

    feat(v3.2.0): add typed environment program data structures and core tests

---
+++ virtual_shaping_lab/vsl/program/__init__.py
@@ -0,0 +1,11 @@
+"""V3 environment-program data structures."""
+
+from .types import EnvironmentProgram, EnvironmentSegment, EventSpec, TrialSpec
+
+__all__ = [
+    "EventSpec",
+    "TrialSpec",
+    "EnvironmentSegment",
+    "EnvironmentProgram",
+]
+
